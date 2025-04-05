581f7c2b7ee6ed338937a7b913f437640000ccae
Lina Shyshova <lina.shyshova@booking.com>
2025-04-05 19:45:59 +0200

    Assignment 1

---
+++ assignment_1/assignment_1.R
@@ -0,0 +1,25 @@
+# Read data
+data <- read.csv("assignment_1/tritium.csv")
+attach(data)
+
+# Plot data
+plot(longitude, latitude, col=2, pch=20, cex=0.5, ylim=c(0,60), xlim=c(-100,10))
+library(rworldmap)
+plot(getMap(), asp = 1, add=T, col="gray60")
+
+# Question 1: Kernel density estimator using Gaussian kernel
+n = nrow(data)
+n
+
+h <- sd(tritium)*(4/(3*n))^(1/5)
+h
+
+d <- density(tritium, bw=h, kernel = "gaussian")
+d
+
+plot(d$x, d$y, type="l", xlab="x", ylab="kernel density")
+points(tritium, rep(0,n), pch = 3)
+
+# Question 2: Confidence interval
+
+# Question 3: Kernel density estimator using Epanechnikov kernel
