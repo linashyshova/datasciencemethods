a8b6674d578186e553e6750961ace66cc151b26e
Cinque033 <156028867+Cinque033@users.noreply.github.com>
2025-04-15 14:23:03 +0200

    Update assignment_2.R question 4 and 5 added

--- assignment_1_2/assignment_2.R
+++ assignment_1_2/assignment_2.R
@@ -40,3 +40,52 @@ plot(x,y, xlab = "Pressure", ylab = "Tritium", col="gray50")
 points(NWe$x,NWe$y,type = "l",col=2,lwd=2)
 points(NWe$x,CI_up,type = "l",col=2,lwd=2,lty=2)
 points(NWe$x,CI_lo,type = "l",col=2,lwd=2,lty=2)
+
+# Question 4: Local quadratic regression
+
+xval <- seq(min(x), max(x), length.out = 802)
+h <- 0.15
+lqest <- rep(NA, length(xval)) 
+               
+for (i in 1:length(xval)) {
+  z <- x - xval[i]
+  w <- dnorm(z / h)  
+  wls <- lm(y ~ z + I(z^2), weights = w)
+  lqest[i] <- wls$coef[1]
+}
+
+plot(x, y, pch = 16, col = "gray60",
+     xlab = "Pressure", ylab = "Tritium", 
+     main = "Local Quadratic Regression (p = 2)")
+lines(xval, lqest, col = 2, lwd = 2)
+
+# Question 5: Local quadratic regression with first and second derivative
+
+xval <- seq(min(x), max(x), length.out = 802)
+h <- 0.15
+
+lqest <- rep(NA, length(xval))       # function estimate
+lqest_prime <- rep(NA, length(xval)) # first derivative
+lqest_double_prime <- rep(NA, length(xval)) # second derivative
+
+for (i in 1:length(xval)) {
+  z <- x - xval[i]
+  w <- dnorm(z / h)
+  wls <- lm(y ~ z + I(z^2), weights = w)  
+  coefs <- coef(wls)
+  
+  lqest[i] <- coefs[1]                      # β₀
+  lqest_prime[i] <- coefs[2]                # β₁ (1st derivative)
+  lqest_double_prime[i] <- 2 * coefs[3]     # 2·β₂ (2nd derivative)
+}
+
+plot(x, y, pch = 16, col = "gray60",
+     xlab = "Pressure", ylab = "Tritium", 
+     main = "Local Quadratic Regression with Derivatives")
+lines(xval, lqest, col = "blue", lwd = 2)
+lines(xval, lqest_prime, col = "red", lwd = 2)
+lines(xval, lqest_double_prime, col = "green", lwd = 2)
+legend("topright", legend = c("Regression", "First Derivative", "Second Derivative"),
+       col = c("blue", "red", "green"), lwd = 2)
+
+               
