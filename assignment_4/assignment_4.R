76c463d97a8474ef335290b65a95487099d42c99
noortje00 <153601022+noortje00@users.noreply.github.com>
2025-05-07 17:05:22 +0200

    Added question 3 and changes to 2

--- assignment_4/assignment_4.R
+++ assignment_4/assignment_4.R
@@ -17,9 +17,6 @@ plot(x, y, pch = 16, col = "gray", main = "Logistic Regression", xlab = "Alcohol
 lines(x_grid, pred, col = "blue", lwd = 2)
 
 # Question 2 - non-parametric logistic regression
-
-x <- alcohol
-y <- quality
 est_s <- gam(y ~ s(x), family = binomial)
 
 # Predicting on the grid
@@ -28,17 +25,30 @@ pred <- predict(est_s, xval, se.fit = TRUE)
 
 # Extract predicted log-odds and compute 95% confidence intervals
 pred_val <- pred$fit
-pred_up <- pred_val + qnorm(0.975) * pred$se.fit
-pred_lo <- pred_val + qnorm(0.025) * pred$se.fit
+pred_up <- pred_val + qnorm(0.95) * pred$se.fit
+pred_lo <- pred_val + qnorm(0.05) * pred$se.fit
 
 # Logistic transformation to get probabilities from log-odds
 prob_fit <- exp(pred_val) / (1 + exp(pred_val))
 prob_up  <- exp(pred_up)  / (1 + exp(pred_up))
 prob_lo  <- exp(pred_lo)  / (1 + exp(pred_lo))
 
-plot(x, y, ylab = "High Quality", xlab = "Alcohol", pch = 20, col = rgb(0,0,0,0.2))
+plot(x, y, ylab = "Conditional probability y=1", xlab = "Alcohol", pch = 20, col = rgb(0,0,0,0.2))
 points(xval$x, prob_fit, type = "l", col = 2, lwd = 2)
 points(xval$x, prob_up,  type = "l", col = 2, lwd = 2, lty = 2)
 points(xval$x, prob_lo,  type = "l", col = 2, lwd = 2, lty = 2)
 
-# Question 3
+# Question 3 - point prediction
+pred_point <- predict(est_s, newdata = data.frame(x = 9.5), se.fit = TRUE)
+
+# Extract predicted log-odds and compute 95% confidence intervals
+pred_point_fit <- pred_point$fit
+pred_point_se  <- pred_point$se.fit
+
+pred_point_up <- pred_point_fit + qnorm(0.95) * pred_point_se
+pred_point_lo <- pred_point_fit + qnorm(0.05) * pred_point_se
+
+# Logistic transformation to get probabilities from log-odds
+prob_point_fit <- exp(pred_point_fit) / (1 + exp(pred_point_fit))
+prob_point_up  <- exp(pred_point_up)  / (1 + exp(pred_point_up))
+prob_point_lo  <- exp(pred_point_lo)  / (1 + exp(pred_point_lo))
